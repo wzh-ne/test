f48e3e7deb3ed9f3e49791c3cc4ae6863964b4d3
wzh_ne <1141565345@qq.com>
2024-04-26 19:52:41 +0800

    提交修改

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to wzh.zzzzz"/>
+    <HelloWorld msg="Welcome to wzh"/>
   </div>
 </template>
 
